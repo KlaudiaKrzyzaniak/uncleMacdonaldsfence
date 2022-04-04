97dfe111e688b3e826fd4f33420fe95f4c1cdf43
KlaudiaKrzyzaniak <102996336+KlaudiaKrzyzaniak@users.noreply.github.com>
2022-04-04 22:55:48 +0200

    changed syntax

--- McdonaldfenceKrzyzaniak.cpp
+++ McdonaldfenceKrzyzaniak.cpp
@@ -13,28 +13,21 @@ int main()
 	
 	cout <<"Please enter RUN:"; // first step to start calculation
 	cin >> initialString;		// user have to write RUN
-	if(initialString!="RUN")	// RUN is obligatory so lets start function IF
-	{
-	cout<<"to proceed you have to write RUN";
-	return 0;
-	}
+		if(initialString!="RUN"){	// RUN is obligatory so lets start function IF
 	
-	else
-	{
-	
-	cout <<"enter the length of the fence[meters]:\n";
-	cin >> circuit; 
+		cout<<"to proceed you have to write RUN";
+		}
+		else{
+		cout <<"enter the length of the fence[meters]:\n";
+		cin >> circuit; 
 
-	
-
-	if (circuit<3)
-	{cout<<"above data should be biger than 2" ;	// data vaidation
-	return 0;
-	}
-	else
-	{cout<<"number is correct\n";					//proper calculation
-	a=1;
-	b=circuit-2*a;              
+			if (circuit<3){
+			cout<<"above data should be number bigger than 2" ;	// data vaidation
+			}
+			else
+			{cout<<"number is correct\n";					//proper calculation
+			a=1;
+			b=circuit-2*a;              
 	
 			//cout <<"a:";
 			//cout <<a<<endl;
@@ -42,33 +35,31 @@ int main()
 			//cout <<"b:";
 			//cout<< b<<endl;
 		
-	while(b>=1)										//loop assumption :width can't be smaller than 1
-	{
-	area= a*b;										
-			//cout << "area for a:";
-			//cout <<a<<endl;
-			//cout <<area<<endl;
-		if(area>optimalArea)						// compare to new data
-		{
-		optimalArea=area;							
-		optimala=a;
-		optimalb=b;
-		}
-	a=a+1;
-	b= b-2;
+				while(b>=1){								//loop assumption :width can't be smaller than 1
+				area= a*b;										
+						//cout << "area for a:";
+						//cout <<a<<endl;
+						//cout <<area<<endl;
+					if(area>optimalArea){					// compare to new data
 		
-	}
-	cout <<"optimal Area [meters^2]:";				// data for user - data for the biggest area uncle Mcdonalds farm
-	cout << optimalArea <<endl;
-	cout <<"optimal a [meters]:";
-	cout << optimala<<endl;
-	cout <<"optimal b [meters]:";
-	cout << optimalb <<endl;
+					optimalArea=area;							
+					optimala=a;
+					optimalb=b;
+					}
+					a=a+1;
+					b= b-2;
+		
+				}
+		cout <<"optimal Area [meters^2]:";				// data for user - data for the biggest area uncle Mcdonalds farm
+		cout << optimalArea <<endl;
+		cout <<"optimal a [meters]:";
+		cout << optimala<<endl;
+		cout <<"optimal b [meters]:";
+		cout << optimalb <<endl;
 
-	}                                    
-}
+			}                                    
+		}
 
-return 0;
 }
  	 
  
